c20ccc8dd9924609fcad6f69fb540bc9cb45c4c5
俊华 蔡 <1032031912@qq.com>
2020-08-06 11:48:23 +0800

    里氏转换与抽象工厂

---
+++ 03_里氏转换/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _03_里氏转换
+{
+    //父类
+    class Friut
+    {
+    }
+
+    class Apple:Friut
+    {
+        public void EatApple()
+        {
+            Console.WriteLine("苹果的吃法");
+        }
+    }
+
+    class Banana : Friut
+    {
+        public void EatBanana()
+        {
+            Console.WriteLine("香蕉的吃饭");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region 里氏转换
+            /* 
+            //如果有一个地方需要父类作为参数那么可以传入子类
+            Friut fr = new Apple();
+            //里氏转换
+            Apple ap = (Apple)fr;
+
+            ap.EatApple();
+            */
+            #endregion
+
+            #region is
+            ////is
+            //Friut fr = new Banana();
+
+            //if (fr is Apple)
+            //{
+            //    Apple ap = (Apple)fr;
+            //    //调用功能函数
+            //    ap.EatApple();
+            //}
+            //else
+            //{
+            //    Console.WriteLine("转换失败");
+            //}
+            #endregion
+
+            #region as
+            //Friut fr = new Banana();
+            //Banana ba = fr as Banana;
+            //Apple ap = fr as Apple;
+
+            //if (ap != null){ ap.EatApple();}
+            //if (ba!=null) { ba.EatBanana();}
+            #endregion
+        }
+    }
+}
---
+++ 04_抽象工厂/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _04_抽象工厂
+{
+    //2.创建具体的产品
+    class Friut
+    {
+        public string color;
+    }
+    class Apple:Friut
+    {
+        public void EatAppple()
+        {
+            Console.WriteLine("我是苹果的吃法");
+        }
+    }
+    class Banana : Friut
+    {
+        public void EatBanana()
+        {
+            Console.WriteLine("我是香蕉的吃法");
+        }
+    }
+    class Pear : Friut
+    {
+        public void EatPear()
+        {
+            Console.WriteLine("我是梨子的吃法");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("请输入您需要吃的水果 1.苹果 2.香蕉 3.梨子");
+
+            //接收结果
+            string result = Console.ReadLine();
+
+            //1.创建抽象产品类 ->2.创建水果类
+            Friut fr = null;
+
+            //3.创建调用工厂
+            switch (result)
+            {
+                case "苹果":
+                    fr = new Apple();
+                    break;
+                case "梨子":
+                    fr = new Pear();
+                    break;
+                case "香蕉":
+                    fr = new Banana();
+                    break;
+            }
+
+            //4.调用工厂类
+            if (fr is Apple)
+            {
+                ((Apple)fr).EatAppple();
+            }
+            if (fr is Banana)
+            {
+                ((Banana)fr).EatBanana();
+            }
+            if (fr is Pear)
+            {
+                ((Pear)fr).EatPear();
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
